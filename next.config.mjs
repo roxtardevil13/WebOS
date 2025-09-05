d06bffad11857ed04015fc40e9c35cabd3ac23ca
roxtardevil13 <61472798+roxtardevil13@users.noreply.github.com>
2025-09-05 12:37:37 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export' };
+const nextConfig = {
+  output: 'export',           // <-- replaces `next export`
+  images: { unoptimized: true } // helpful for static hosting
+};
+
 export default nextConfig;
